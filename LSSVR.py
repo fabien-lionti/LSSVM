f0186b81d4037a14bc7961e9b37ea8aefa678884
Fabien Lionti <fabien.lionti@gmail.com>
2020-03-15 20:09:04 +0100

    Add plot

--- LSSVR.py
+++ LSSVR.py
@@ -94,10 +94,16 @@ def test():
 
     # Training
 
-    lssvr = LSSVR(gamma=10, kernel_name="rbf", sigma=1)
+    lssvr = LSSVR(gamma=100, kernel_name="rbf", sigma=1)
     lssvr.fit(X_train, y_train)
     y_pred = lssvr.predict(X_test)
     rms = lssvr.rmse(y_pred,y_test)
     print('Error : '+str(rms))
+
+    plt.figure(figsize=(10,10))
+    plt.plot(X_train, y_train, '.')
+    plt.plot(X_test, y_pred, '.')
+    plt.show()
+
 
 test()
